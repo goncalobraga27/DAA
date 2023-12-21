c49b380403ca6c90b8a02731cc239f330bb72f71
goncalobraga27 <a97541@alunos.uminho.pt>
2023-12-21 17:31:05 +0000

    Radiação

---
+++ src/parte2/requestData/requestApi_radiation_Train.py
@@ -0,0 +1,45 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 41.5518,
+	"longitude": -8.4229,
+	"start_date": "2021-09-01",
+	"end_date": "2022-12-31",
+	"daily": "shortwave_radiation_sum",
+	"timezone": "GMT"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_shortwave_radiation_sum = daily.Variables(0).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s"),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["shortwave_radiation_sum"] = daily_shortwave_radiation_sum
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+daily_dataframe.to_csv('../../../datasets/parte2/treino/radiation.csv', index=False)
